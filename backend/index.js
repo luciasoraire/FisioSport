9d6bc3f746745af495f6b135e1123a411554406f
Mariano Torres <marianxtorres@gmail.com>
2023-12-06 21:19:52 -0300

    Automatización y Filtros Combinados

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const { sequelize } = require('./db')
 const { encrypt } = require('./src/utils/passwordEncrypt')
 const {PORT} = process.env || 3001
 const { User } = require('./db')
+require('./cronJob')
 
 const app = express()
 
@@ -14,7 +15,7 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.use('/fisiosport', router) 
-
+ 
 sequelize.sync({ alter: true }).then(async() => {  
     const admin = await User.findOne({ where: { email: 'admin@gmail.com' } });
         if (!admin) { 
