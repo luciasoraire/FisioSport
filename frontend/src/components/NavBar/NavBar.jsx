600a100e79c31a51de576f89acfad96ff5c862fb
Lucia Soraire <luciasoraire@gmail.com>
2024-02-22 22:08:22 -0300

    Agregué botón de cerrar sesión

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Logo from './../../assets/logoazul.png'
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./NavBar.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MdOutlineDashboard } from "react-icons/md";
+import { closeSesion } from "../../Redux/Actions/Actions";
+import Cookies from 'js-cookie';
+
+
 const NavBar = () => {
+
     const [clicked, setClicked] = useState(false);
     const userAuth = useSelector(state => state.userAuth)
-    console.log(userAuth);
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
     const handleClick = () => {
         setClicked(!clicked);
     }
 
     const handleLogout = () => {
         dispatch(closeSesion())
+        Cookies.remove('token');
+        navigate('/')
     }
 
     return (
@@ -37,16 +46,21 @@ const NavBar = () => {
                         </li>
                      
                        
-                            {
-                               !userAuth.authenticated && <li className="itemnavbar" id="iniciarsesion"><NavLink to="/login" activeClassName="active"><i class="fa-solid fa-user"></i> Iniciar Sesión</NavLink></li>              
-                            }
+                      
                             {   
-                                userAuth.authenticated && !userAuth.isAdmin && <li className="itemnavbar"><i class="fa-solid fa-user"></i><NavLink to="/info" activeClassName="active">Información Personal</NavLink></li>
+                                userAuth.authenticated && !userAuth.isAdmin && <li className="itemnavbar"><i class="fa-solid fa-circle-info"></i><NavLink to="/info" activeClassName="active">Información Personal</NavLink></li>
                             }
                             {   
                                 userAuth.isAdmin && <li className="itemnavbar">
                                     <i class="fa-solid fa-table"></i> <NavLink to="/admin" activeClassName="active">Dashboard</NavLink>
                             </li>}
+                            {
+                               !userAuth.authenticated 
+                               ? <li className="itemnavbar"><NavLink to="/login" activeClassName="active"><i class="fa-solid fa-user"></i> Iniciar Sesión</NavLink></li>              
+                                : <li className="itemnavbar" id="cerrarsesion" onClick={handleLogout}>
+                                <NavLink to="/login" ><i class="fa-solid fa-user"></i> Cerrar Sesión</NavLink>
+                              </li>       
+                            }
                             
                        
 
