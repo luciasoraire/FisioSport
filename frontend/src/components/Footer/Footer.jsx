b00efde3a9033427b5b9fc5809ae8b01d2a401c2
Lucia Soraire <luciasoraire@gmail.com>
2024-02-27 22:20:19 -0300

    Footer responsive

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,16 +9,59 @@ const Footer = () => {
 
     return (
         <>
-            <footer>
+        <div className="footer">
+    <footer>
+      <div class="top">
+        <div class="pages">
+          <ul>
+            <h3>Explore</h3>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Iniciar Sesión</a></li>
 
-            <div className="iconsfooter">
-            <a href="https://www.instagram.com/biomecanicatucuman" target="_blank" rel="noopener noreferrer"> <i class="fa-brands fa-instagram" id="iconfooter"></i></a>
-            <a href="https://www.instagram.com/fisiosport.tucuman" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook" id="iconfooter"></i></a>
-            <Link to="/contacto"><i class="fa-solid fa-envelope" id="iconfooter"></i></Link>
-            </div>
-            <b><p>Kin. Christian Fabián Núñez Coso</p></b>
-            <p>©Todos los derechos reservados.</p>
+          </ul>
+
+          <ul>
+            <h3>Conozca</h3>
+            <li><a href="#">Sobre Nosotros</a></li>
+            <li><a href="#">Reseñas</a></li>
+            <li><a href="#">Ubicación</a></li>
+          </ul>
+
+          <ul>
+            <h3>Obras Sociales</h3>
+            <li><a href="#">PAMI</a></li>
+            <li><a href="#">Boreal</a></li>
+
+          </ul>
+        </div>
+        <div class="newsletter">
+          <h3>NEWSLETTER</h3>
+          <form>
+            <input
+              type="email"
+              name="newsletter_email"
+              id="newsletter_email"
+              placeholder="Email"
+            />
+            <input type="button" value="Enviar" />
+          </form>
+        </div>
+      </div>
+      <div class="social">
+        <i class="fab fa-github"></i>
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-google"></i>
+      </div>
+      <div class="info">
+        <div class="legal">
+          <a href="#">Términos y Condiciones</a><a href="#">Política de Privacidad</a>
+        </div>
+        <div class="copyright">2024 &copy; Tesis Soraire, Lucía - Torres, Mariano</div>
+      </div>
     </footer>
+    </div>
 
         </>
     );
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home">
       <div className="titulovideo">
         <video src={Intro} className="videointro" autoPlay loop />
-       {/* <Link to="/turno" className="titulosobrevideo">
-          <h1>Solicite su turno</h1>
-  </Link>*/}
+        <div className="botonturno">
+
+</div>
 
       </div>
 
