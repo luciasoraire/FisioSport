722ce6b33cba4e8fb144c6eea0bbd6500036793f
Lucia Soraire <luciasoraire@gmail.com>
2024-02-29 18:56:11 -0300

    Cambié NavBar, Footer y añadí botón sobre el video

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import "./HomePage.css";
-import Intro from "./../../assets/intro.mp4";
+import Intro from "./../../assets/intro2.mp4";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
@@ -7,13 +7,20 @@ const HomePage = () => {
     <div className="home">
       <div className="titulovideo">
         <video src={Intro} className="videointro" autoPlay loop />
-        <div className="botonturno">
+        <div className="intro">
+          <h1 className="fisiosport">FISIO SPORT</h1>
+          <h2 className="kinesiologiayfisioterapia">Kinesiología & Fisioterapia</h2>
+          <i><h3 className="kinesiologo">Klgo. Christian Núñez Coso</h3></i>
+          <button class="button">
+  <span class="button-content">Solicitar turno</span>
+</button>
+        </div>
 
 </div>
 
       </div>
 
-    </div>
+  
   );
 };
 
